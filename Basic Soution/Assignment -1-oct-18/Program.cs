fb40d587ed3e4b09926e106a0fdbd9f8c141f9ee
vikash281 <vikash.kumar7@ust.com>
2023-10-18 17:37:22 +0530

    Assignment-1

---
+++ Basic Soution/Assignment -1-oct-18/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Assignment__1_oct_18;
+
+Student student = new("vikash", 50, 59, 60);
+Console.Write("Name = ");
+Console.WriteLine(student.name);
+Console.Write("Grade = ");
+Console.WriteLine(student.calculateAverage());
---
+++ Basic Soution/Assignment -1-oct-18/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment__1_oct_18
+{
+    public class Student
+    {
+        public string name;char grades;
+        public int marks1, marks2, marks3;
+
+        public Student(string name,int marks1,int marks2,int marks3)
+        {
+            this.name = name;
+            this.marks1 = marks1;
+            this.marks2 = marks2;
+            this.marks3 = marks3;
+        }
+
+        public char calculateAverage()
+        {   double Average=(marks1 + marks2 +marks3)/3;
+
+            if(Average > 89)
+            {
+                return 'A';
+            }
+            if(Average > 79)
+            {
+                return 'B';
+            }
+            if (Average > 69)
+            {
+                return 'C';
+            }
+            if (Average > 59)
+            {
+                return 'D';
+            }
+           return 'E';
+        }
+    }
+}
