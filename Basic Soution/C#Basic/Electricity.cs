7645632159a6b3f8d7dd2231bc4156606288952b
vikash281 <vikash.kumar7@ust.com>
2023-10-18 23:42:37 +0530

    Constructor example

---
+++ Basic Soution/C#Basic/Electricity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Basic
+{
+    public class Electricity
+    {
+      public  int consumernumber, prevoiusReading, currentReading;
+       public     string? consumerName;
+
+        public Electricity(int consumernumber, int prevoiusReading, int currentReading, string? consumerName)
+        {
+            this.consumernumber = consumernumber;
+            this.prevoiusReading = prevoiusReading;
+          this.currentReading = currentReading;
+            this.consumerName = consumerName;
+        }
+
+        /*public  Electricity() 
+        {
+            consumernumber = 23456;
+            prevoiusReading = 9000;
+            CurrentReading = 9300;
+            consumerName = "vikash";
+        }*/
+
+
+        public double CalculateNumber()
+        {
+            double billamount;
+            int reading = currentReading - prevoiusReading;
+            if (reading<=100)
+            {
+                billamount = reading * 2.00;
+                
+            }
+            else if(reading<=201 )
+            {
+                billamount = 100 * 2 + (reading-100)*2.5;
+            }
+            else if (reading<=300)
+            {
+                billamount = 100 * 2 + 100 * 2.5 + (reading - 200) * 4;
+            }
+            else
+            {
+                billamount = 100 * 2 + 100 * 2.5 + 100*4 +(reading - 300) * 5;
+            }
+            return billamount;
+        }
+
+    }
+}
--- Basic Soution/C#Basic/Program.cs
+++ Basic Soution/C#Basic/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using C_Basic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       /* Console.WriteLine("Hello, World!");*/
+        Electricity electricity = new Electricity(12345,9000,9300,"vikash");
+      Console.WriteLine(electricity.CalculateNumber());
+        Console.WriteLine(electricity.consumerName);
+    }
+}
