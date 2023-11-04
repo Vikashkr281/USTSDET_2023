d9f85a593d8f1b2d42dfb788457bbddbdfc0d739
vikash281 <vikash.kumar7@ust.com>
2023-11-04 17:38:39 +0530

    casestudy2

---
+++ Basic Soution/CASESTUDY_2_04_Nov/Exception/CourcesException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CASESTUDY_2_04_Nov.Exception1
+{
+    
+        internal class CourceException : Exception
+        {
+            public CourceException(string message) : base(message)
+            {
+
+            }
+        }
+    
+}
---
+++ Basic Soution/CASESTUDY_2_04_Nov/IEnrolable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CASESTUDY_2_04_Nov
+{
+
+        internal interface IEnrolable
+        {
+            void CourseRegistration();
+            void Withdrawal();
+        }
+    }
